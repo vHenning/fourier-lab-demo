d3877557ac3816d795dab9c24e915f3361253b1b
Henning Vehring <henning.vehring@gmail.com>
2023-10-31 14:27:54 -0600

    Add stacked squares aperture

---
+++ labDemo.m
@@ -0,0 +1,18 @@
+numberSquares = 3;
+squareSize = 0.5;
+squareDistance = 5;
+
+[N, M, Q, L, l] = getParameters(2, 10, 633e-6, 1000);
+
+Q = 16;
+N = M * Q;
+L = l * Q;
+
+aperture = zeros(N);
+aperture(floor(N/2) - floor(M/2):floor(N/2) + floor(M/2), floor(N/2) - floor(M/2):floor(N/2) + floor(M/2)) = 1;
+
+aperture(floor(N/2) - M:floor(N/2) - floor(M/2), floor(N/2) - floor(M/4):floor(N/2) + floor(M/4)) = 1;
+aperture(floor(N/2) - M - floor(M/4):floor(N/2) - M, floor(N/2) - floor(M/8):floor(N/2) + floor(M/8)) = 1;
+
+imshow(aperture);
+
