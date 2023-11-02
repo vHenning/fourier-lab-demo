ff13f29580e6afab26b24a36bd5f37a80c633937
Henning Vehring <henning.vehring@gmail.com>
2023-11-01 22:10:05 -0600

    Move aperture build to function

--- labDemo.m
+++ labDemo.m
@@ -3,20 +3,25 @@ squareSize = 0.5;
 squareDistance = 5;
 
 wavelength = 633e-6;
-distance = 10000;
+distance = 1000;
 apertureSize = 10;
 
 [N, M, Q, L, l] = getParameters(2, apertureSize, wavelength, distance);
 
-Q = 16;
-N = M * Q;
-L = l * Q;
+% Q = 16;
+% N = M * Q;
+% L = l * Q;
 
-aperture = zeros(N);
-aperture(floor(N/2) - floor(M/2):floor(N/2) + floor(M/2), floor(N/2) - floor(M/2):floor(N/2) + floor(M/2)) = 1;
+aperture = getStackedSquares(N, M);
 
-aperture(floor(N/2) - M:floor(N/2) - floor(M/2), floor(N/2) - floor(M/4):floor(N/2) + floor(M/4)) = 1;
-aperture(floor(N/2) - M - floor(M/4):floor(N/2) - M, floor(N/2) - floor(M/8):floor(N/2) + floor(M/8)) = 1;
+[x, y, uz, Iz] = diffraction(aperture, L, wavelength, distance);
 
-imshow(aperture);
+imshow(Iz);
 
+function aperture = getStackedSquares(N, M)
+    aperture = zeros(N);
+    aperture(floor(N/2) - floor(M/2):floor(N/2), floor(N/2) - floor(M/2):floor(N/2) + floor(M/2)) = 1;
+    
+    aperture(floor(N/2) - M:floor(N/2) - floor(M/2), floor(N/2) - floor(M/4):floor(N/2) + floor(M/4)) = 1;
+    aperture(floor(N/2) - M - floor(M/4):floor(N/2) - M, floor(N/2) - floor(M/8):floor(N/2) + floor(M/8)) = 1;
+end
