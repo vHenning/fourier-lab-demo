fbcf8f65b4ac103a1a72ab715d5e6d985ff74684
Henning Vehring <henning.vehring@gmail.com>
2023-11-01 22:43:57 -0600

    cut zero padding

--- labDemo.m
+++ labDemo.m
@@ -8,7 +8,15 @@ aperture = getTriangle(L, l, N);
 
 [x, y, uz, Iz] = diffraction(aperture, L, wavelength, distance);
 
-imshow(Iz);
+cutAmountPerSide = floor((N-M) * 0.95 / 2);
+apertureCut = aperture(cutAmountPerSide:N-cutAmountPerSide, cutAmountPerSide:N-cutAmountPerSide);
+IzCut = Iz(cutAmountPerSide:N-cutAmountPerSide, cutAmountPerSide:N-cutAmountPerSide);
+
+
+figure(1);
+imshow(apertureCut);
+figure(2);
+imshow(IzCut);
 
 function aperture = getStackedSquares(N, M)
     aperture = zeros(N);
